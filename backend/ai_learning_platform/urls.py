4cdf4d2752b9a5fb42128fb2951123af32ea20cc
YAN Ran <yran0407@gmail.com>
2026-04-07 09:09:38 +0000

    fix: serve media files in production

--- backend/ai_learning_platform/urls.py
+++ backend/ai_learning_platform/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework.authtoken.views import obtain_auth_token
 from .health import health_check
 
@@ -30,3 +31,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # 生产环境兜底提供媒体文件服务（课程封面/附件）
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
